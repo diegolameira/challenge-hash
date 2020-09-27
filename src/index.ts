b021b96229305ce49bb18c3278c1c40bde457af0
Diego Lameira <diegolameira@me.com>
2020-09-27 16:48:14 -0300

    feat(service): call service and update report (#2)
    * feat(service): add api service
    
    * feat(service): call service and update report

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,41 @@
 import { Template } from './template'
+import { service } from './service'
 import './styles.scss'
 function render() {
   const appWrapper = document.getElementById('app')
   if (appWrapper) {
     appWrapper.innerHTML = Template
   }
+  const days = [1, 15, 30, 90]
+
+  const form = document.querySelector('form')
+  if (form)
+    form.addEventListener(
+      'blur',
+      () => {
+        const data: any = {}
+
+        new FormData(form).forEach((v, k) => {
+          data[k] = Number(v)
+        })
+
+        if (Object.values(data).filter(Boolean).length >= 3) {
+          const { amount, installments, mdr } = data
+          service
+            .calculate(amount, installments, mdr, days)
+            .then((res) => {
+              return days.map((v) => res[v])
+            })
+            .then((res) => {
+              const report = form.querySelectorAll('p > strong')
+              report.forEach((el, key) => {
+                el.innerHTML = res[key]
+              })
+            })
+        }
+      },
+      true,
+    )
 }
 
 render()
